53e5e52c86880e27945ad15b39d746c22b7d3119
Abdul azeez <meazeez@gmail.com>
2022-10-08 13:24:23 +0530

    Users Should be able to mark a Task Complete

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::controller(TaskController::class, function () {
-    
-    Route::get('get/parent-tasks','create');
-    Route::post('tasks/store','store');
 
-    Route::delete('tasks/{id}/destroy','destroy');
+    Route::get('get/parent-tasks', 'create');
+    Route::post('tasks/store', 'store');
 
+    Route::delete('tasks/{id}/destroy', 'destroy');
 
+    //completed url
+    Route::put('tasks/{id}/completed', 'completed');
 });
